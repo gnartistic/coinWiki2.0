94100e925b83903581d8da26f973caac704870e8
charlie <gn4rtistic@gmail.com>
2022-06-19 02:34:28 -0700

    added some functionality to coins page

--- assets/js/coins.js
+++ assets/js/coins.js
@@ -1,10 +1,44 @@
 // html variables
+var trendingContainer = $("#trendContain");
+// coingecko coin list api
+function coinsTrendingapi() { // ref line 19
+    fetch(`https://api.coingecko.com/api/v3/search/trending`) // fetch url with dynamic endpoint
+        .then(function (response) {
+            response.json()
+                .then(function (data) {
+                    console.log(data.coins);
+                    trends = data.coins;
+                    // setting parameters for the following functions
+                    displayTrending(trends);
+                })
+        });
+
+    return;
+}
+
+function displayTrending(trends) { // ref coinpaprikaTweetApi
+    trendingContainer.html(`<h3 class="title is-size-1">Trending Coins</h3>`); // section title
+    for (var i = 0; i <= trends.length; i++) { // for loop through data ie 'tweets'
+        var divEl1 = $(`
+        <div class="tile" 
+            <div class="tile content">
+                <div class="box">
+                    <h3 class="title">${trends[i].item.name}</h3>
+                    <h4 class="subtitle">${trends[i].item.symbol}</h4>
+                </div>
+            </div>
+        </div>`)
+        divEl1.appendTo(trendingContainer) // place inside container already created in html file
+}
+
+}
+coinsTrendingapi(); // yeet
+
 var searchInputEl = $("#searchInput");
 var searchFormEl = $("#searchForm");
 var coinDataContainer = $("#coinDataContainer");
-var searchHistoryEl = $("#previousSearches");
 var coinPriceEl = $("#coinPrice");
-var searchHistoryContainer = $("#searcHistoryContainer");
+
 //search function
 function searchInput(event) { // use the submit event
     event.preventDefault()
@@ -22,14 +56,11 @@ function coinsListapi(coinName) { // ref line 19
                     coinSymbol = data.symbol; // ex: {"symbol: 'btc'"} this is the coin's ticker
                     coinDescription = data.description.en; // ex {"description: en: (some text)"} the description
                     coinHomepage = data.links.homepage[0]; // homepage link
-                    coinlink1 = data.links.blockchain_site[0]; // blockchain site
-                    coinlink2 = data.links.official_forum_url[0]; // forum site
                     coinlink3 = data.links.subreddit_url; // subreddit
 
                     // setting parameters for the following functions
-                    displayCurrentData(coinName, coinIcon, coinSymbol, coinDescription, coinHomepage, coinlink1, coinlink2, coinlink3);
+                    displayCurrentData(coinName, coinIcon, coinSymbol, coinDescription, coinHomepage, coinlink3);
                     PriceData(coinId);
-                    coinPaprikaTweetApi(coinSymbol, coinId);
                 })
         });
 
@@ -45,7 +76,7 @@ function PriceData(coinId) { // ref coinsListapi
                     coinPrice = data[0].current_price;
                     coinName = data.name;
                     // set new parameters with current parameters
-                    displayCurrentData(coinName, coinIcon, coinSymbol, data[0].current_price, coinDescription, coinHomepage, coinlink1, coinlink2, coinlink3);
+                    displayCurrentData(coinName, coinIcon, coinSymbol, data[0].current_price, coinDescription, coinHomepage, coinlink3);
 
                 });
         });
@@ -53,16 +84,13 @@ function PriceData(coinId) { // ref coinsListapi
     return;
 }
 
-function displayCurrentData(coinName, coinIcon, coinSymbol, coinPrice, coinDescription, coinHomepage, coinlink1, coinlink2, coinlink3) { // ref coinsListapi
-    $("#coinDataContainer").attr("class", "card cr col s5 hoverable");// coin data container in html file
+function displayCurrentData(coinName, coinIcon, coinSymbol, coinPrice, coinDescription, coinHomepage, coinlink3) { // ref coinsListapi
+    $("#coinDataContainer").attr("class", "card is-ghost");// coin data container in html file
     $("#coinName").html(coinName);// coin name
     $("#coinSymbol").html(coinSymbol); // coin symbol
     $("#coinHomepage").attr("href", `${coinHomepage}`) // link to homepage
-    $("#coinHomepage").attr("class", "btn-floating btn-large pulse"); // icon pulse
     $("#icon").attr("src", `${coinIcon}`); //icon
     $("#description").html(coinDescription); //description
-    $("#link1").attr("href", `${coinlink1}`); // link to blockchain site
-    $("#link2").attr("href", `${coinlink2}`); // link to coin forum site
     $("#link3").attr("href", `${coinlink3}`); // link to subreddit
 
     // number formatter.
@@ -73,50 +101,11 @@ function displayCurrentData(coinName, coinIcon, coinSymbol, coinPrice, coinDescr
     var coinPriceDisp = formatter.format(coinPrice); // display coin price in currency format
 
     $("#coinPrice").html(coinPriceDisp);// coin price
-    dispSearchHist(coinId, false);
-}
+} 
 
-
-function dispSearchHist(coinId, initialStart) {
-    // search history
-    var matchFound = false;
-    $("#previousSearches").children("").each(function () {
-        if (coinId == $(this).text()) {
-            matchFound = true;
-            return;
-        }
-    });
-    if (matchFound) { return; }
-
-    var buttonEl = $('<button type="button" class="col s12 m3 btn">' + coinId + '</button>')
-    buttonEl.on("click", previousButtonClick);
-    buttonEl.prependTo(searchHistoryEl);
-
-    if (!initialStart) { savePreviousData(coinId) };
-}
-
-function savePreviousData(coinId) {
-    tempItem = JSON.parse(localStorage.getItem("previousSearches"))
-    if (tempItem != null) {
-        localStorage.setItem("previousSearches", JSON.stringify(tempItem.concat(coinId)))
-    } else {
-        tempArr = [coinName];
-        localStorage.setItem("previousSearches", JSON.stringify(tempArr))
-    }
-}
-
-function previousButtonClick(event) {
-    coinsListapi(event.target.innerHTML);
-}
 
 function start() { // lets get this shit started frfr
     searchFormEl.submit(searchInput)
-    tempArr = JSON.parse(localStorage.getItem("previousSearches"))
-    if (tempArr != null) {
-        for (let i = 1; i < tempArr.length; i++) {
-            dispSearchHist(tempArr[i], true);
-        }
-    }
 }
 
-start() // yeet
+start();
